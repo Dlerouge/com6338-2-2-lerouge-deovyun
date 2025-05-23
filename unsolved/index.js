b5a51d2ad09e03ef6961ddcb73e3a6b9f2f071ea
De'ovyun LeRouge <lerouge966@gmail.com>
2025-05-23 09:50:48 -0400

    Update index.js

--- unsolved/index.js
+++ unsolved/index.js
@@ -1,1 +1,15 @@
 // Your code here
+function greet () {
+    const name = prompt("What is your name?");
+    alert("Hello, " + name)
+    const age = parseInt(prompt("How old are you?"), 10);
+    const hadBirthday = confirm("Have you had your birthday yet this year?");
+    const currentYear = new Date().getFullYear();
+    let birthYear;
+    if (hadBirthday) {
+        birthYear = currentYear - age;
+    } else {
+        birthYear = currentYear - age - 1;
+    }
+    alert("You were born in " + birthYear + ".");
+}
